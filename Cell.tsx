9ce9af85f9e972d9aa82a0c7299709925377aba8
Matt Mullins <mmullins@mmlx.us>
2025-08-17 16:41:01 -0700

    css: make a solved square render one large number
    Also appease the React warning about adding a "key" property that it throws
    while I'm manually testing this.

--- Cell.tsx
+++ Cell.tsx
@@ -5,7 +5,7 @@ import { Cell as WasmCell } from './pkg'
 export default function Cell({ state }: { state: WasmCell }) {
     let children: JSX.Element[] = [];
     if ("Solved" in state) {
-        children = [<div className="solved">{state.Solved}</div>];
+        children = [<div key="solved" className="solved">{state.Solved}</div>];
     } else {
         for (const choice of state.Choices) {
             children.push(<div key={choice} className={"choice-" + choice}>{choice}</div>)
