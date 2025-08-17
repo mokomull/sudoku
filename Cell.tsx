88d9e80cc5cd9646d60212019a663c49957703fb
Matt Mullins <mmullins@mmlx.us>
2025-08-16 23:29:27 -0700

    web: fix rendering of solved Cell
    Silly bug, just needed to actually curly-brace the expression...

--- Cell.tsx
+++ Cell.tsx
@@ -5,7 +5,7 @@ import { Cell as WasmCell } from './pkg'
 export default function Cell({ state }: { state: WasmCell }) {
     let children: JSX.Element[] = [];
     if ("Solved" in state) {
-        children = [<div className="solved">state.Solved</div>];
+        children = [<div className="solved">{state.Solved}</div>];
     } else {
         for (const choice of state.Choices) {
             children.push(<div className={"choice-" + choice}>{choice}</div>)
