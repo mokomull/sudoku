363f93e8136ac871a6a107e8021600ab5814d062
Matt Mullins <mmullins@mmlx.us>
2025-08-24 00:02:53 -0700

    web: add a WTF page

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     react(),
     wasm(),
     topLevelAwait()
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: [
+        resolve(__dirname, "index.html"),
+        resolve(__dirname, "wtf.html"),
+      ]
+    }
+  },
 });
