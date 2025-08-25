dd0256578284ca9b308bf0f0ca9bd0044ca69577
Matt Mullins <mmullins@mmlx.us>
2025-08-24 22:21:45 -0700

    web: add react hooks to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.stylistic,
+  reactHooks.configs['recommended-latest'],
   {
     ignores: [
       "dist/**",
